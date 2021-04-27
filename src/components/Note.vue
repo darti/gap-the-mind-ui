00b80b8f1826c1b7849bd85d9a2180b0eafeb447
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2021-04-27 13:45:41 +0100

    Add tailwind and tiptap

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,10 +1,16 @@
 <template>
-  <editor-content :editor="editor" />
+  <div class="shadow-lg rounded-md">
+    <editor-content :editor="editor" />
+  </div>
 </template>
 
 <script lang="ts">
 import { useEditor, EditorContent } from "@tiptap/vue-3";
 import { defaultExtensions } from "@tiptap/starter-kit";
+import Highlight from '@tiptap/extension-highlight'
+import Typography from '@tiptap/extension-typography'
+import Text from '@tiptap/extension-text'
+import TextAlign from '@tiptap/extension-text-align'
 
 export default {
   components: {
@@ -14,7 +20,10 @@ export default {
   setup() {
     const editor = useEditor({
       content: "<p>I’m running tiptap with Vue.js. 🎉</p>",
-      extensions: defaultExtensions(),
+      extensions: [...defaultExtensions(), Highlight,
+        Typography,
+        Text,
+        TextAlign],
     });
 
     return { editor };
